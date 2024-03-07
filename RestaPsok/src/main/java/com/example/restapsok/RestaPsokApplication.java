67f988089caa3586f8dfa6f49a6ef72100a0e98d
@paglebov_1 <paglebov_1@edu.hse.ru>
2024-03-07 12:58:59 +0300

    first commit

---
+++ RestaPsok/src/main/java/com/example/restapsok/RestaPsokApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapsok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaPsokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaPsokApplication.class, args);
+	}
+
+}
---
+++ RestaPsok/src/test/java/com/example/restapsok/RestaPsokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapsok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaPsokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
